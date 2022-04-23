525ba07c6cbfecf979809f1acec99a1c47a85785
mishra_abhi <abhi200108010@gmail.com>
2022-04-23 18:14:58 +0530

    added signin/signup activity

--- app/src/main/java/com/abhishek/crud_app/MainActivity.java
+++ app/src/main/java/com/abhishek/crud_app/MainActivity.java
@@ -2,7 +2,15 @@ package com.abhishek.crud_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
